900d83f6d3ba3262add4b6996c49a34a30b9a0c1
Edson Machado <edsonjmv@gmail.com>
2021-09-22 20:05:48 +0200

    outputting a list of items

--- App.js
+++ App.js
@@ -24,7 +24,11 @@ export default function App() {
         />
         <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <View>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
